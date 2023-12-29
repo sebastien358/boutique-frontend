f0ff4e77f5f224219ea4908efd8c2b4d545ef4b0
Sébastien Petit <sebastienpetit27330@gmail.com>
2023-12-29 10:37:47 +0100

    update product list admin css img

--- src/features/admin/views/ProductLIst.vue
+++ src/features/admin/views/ProductLIst.vue
@@ -11,7 +11,7 @@
       <ul class="list-inline">
         <li v-for="product in products" :key="product.id" class="d-flex align-items-center flex-row border border-1 px-3 py-2 rounded-2 bg-white mb-3">
           <div class="d-flex align-items-center flex-row flex-fill">
-            <img v-if="product.pictures[0]" :src="product.pictures[0]" height="60" width="90" class="object-fit-cover rounded-2 me-3">
+            <img v-if="product.pictures[0]" :src="product.pictures[0]" height="45" width="55" class="object-fit-cover rounded-2 me-3">
             <img v-else src="@/assets/images/image-not-found.jpg" height="60" width="90" class="object-fit-cover rounded-2 me-2">
             <h3 class="m-0 fs-6 pe-5">{{product.title}}</h3>
             <p class="m-0">{{product.price}} €</p>
